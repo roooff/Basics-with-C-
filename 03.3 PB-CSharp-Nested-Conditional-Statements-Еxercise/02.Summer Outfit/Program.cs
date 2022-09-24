d48b4c77425407e0f9958695925067e57b068f80
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2022-09-24 16:21:06 +0300

    Add files via upload

---
+++ 03.3 PB-CSharp-Nested-Conditional-Statements-Еxercise/02.Summer Outfit/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+            string outfit ="";
+            string shoes = "";
+            if (10<= degrees &&  degrees<= 18)
+            {
+                if (time == "Morning")
+                {
+                    outfit = "SweatShirt";
+                    shoes = "Sneakers";
+                }
+                else if (time== "Afternoon" || time == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (18 < degrees && degrees <= 24)
+                {
+                    if (time == "Afternoon")
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    else if (time == "Morning" || time == "Evening")
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (degrees > 25)
+                    {
+                        if (time == "Morning")
+                        {
+                            outfit = "T-Shirt";
+                            shoes = "Sandals";
+                        }
+                        else if (time == "Afternoon")
+                        {
+                            outfit = "Swim Suit";
+                            shoes = "Barefoot";
+                        }
+                        else if (time == "Evening")
+                        {
+                            outfit = "Shirt";
+                            shoes = "Moccasins";
+                        }
+
+                    }
+                }
+          
+          
+           
+            }
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+        }
+
+    }
+}
---
+++ 03.3 PB-CSharp-Nested-Conditional-Statements-Еxercise/1.Cinema/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            double income = 0.0;
+            switch(type)
+            {
+                case "Premiere":
+                    income = 12.50;
+                    break;
+                case "Normal":
+                    income = 7.50;
+                    break;
+                case "Discount":
+                    income = 5;
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+            double TotalSum = income * rows * columns;
+            Console.WriteLine($"{TotalSum:f2}");
+
+        }
+    }
+}
